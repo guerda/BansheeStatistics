b5956790590d11f436c82f5dad9e408b9617cf4d
Philip Gillißen <guerda@freenet.de>
2013-05-19 14:16:35 +0200

    Rotated labels for better readability

--- BansheeStatistics.R
+++ BansheeStatistics.R
@@ -6,40 +6,45 @@ tables <- dbListTables(con)
 
 countOfArtists <- dbGetQuery(conn=con, statement="SELECT COUNT(1) FROM CoreArtists")
 
-barplotCount <- function(data,file,count=20) {
+barplotCount <- function(data,file,count=20, title) {
   png(file=file, width=800, height=800)
   palette(topo.colors(count))
   ylim <- c(0,max(data[,2])*1.1)
   print(ylim)
-  p <- barplot(data[1:count,2], names.arg=data[1:count,1], legend.text=data[1:count,1], col=topo.colors(count), ylim=ylim, las=2)
-  text(p, data[1:count,2], labels=data[1:count,2],pos=3)
+  p <- barplot(data[1:count,2], names.arg=data[1:count,1], legend.text=data[1:count,1], col=topo.colors(count), ylim=ylim, main=title, xaxt="n")
+  labs <- paste(data[1:count,1])
+  text(p, par("usr")[3]-1, labels=labs, xpd=TRUE, srt=45, adj=1)
+  text(p, data[1:count,2], labels=data[1:count,2],pos=3, mgp=c(3,.5,2))
   graphics.off()
 }
 
 
-genreGroupsSelect <- "Select genre, count(1) as count from coretracks where genre is not null group by genre order by count desc;"
+genreGroupsSelect <- "SELECT t.genre, count(1) AS count FROM CoreTracks t WHERE t.genre IS NOT NULL AND t.PrimarySourceID = 1 GROUP BY t.genre ORDER BY count DESC;"
 genreGroups <- dbGetQuery(conn=con, statement=genreGroupsSelect)
+barplotCount(genreGroups, file="Genre_Groups.png", title="Most Tracks per Genre")
 
-barplotCount(genreGroups, file="Genre_Groups.png")
-
-artistCountSelect <- "select a.name, count(1) as count from coretracks t left join coreartists a on a.artistid = t.artistId where a.name is not null group by t.artistid order by count desc;"
+artistCountSelect <- "SELECT a.name, count(1) AS count FROM coretracks t LEFT JOIN coreartists a ON a.artistid = t.artistId WHERE a.name IS NOT NULL AND t.PrimarySourceID = 1 GROUP BY t.artistid ORDER BY count DESC;"
 artistCount <- dbGetQuery(conn=con, statement=artistCountSelect)
-barplotCount(artistCount, file="Artist_Count.png")
+barplotCount(artistCount, file="Artist_Count.png", title="Artists With the Most Tracks")
 
-
-artistPlayCountSelect <- "select a.name, sum(playcount) as count from coretracks t left join coreartists a on a.artistid = t.artistid where a.name is not null group by t.artistid order by count desc;"
+artistPlayCountSelect <- "SELECT a.name, SUM(playcount) AS count FROM coretracks t LEFT JOIN coreartists a ON a.artistid = t.artistid WHERE a.name IS NOT NULL AND t.PrimarySourceID = 1 GROUP BY t.artistid ORDER BY count DESC;"
 artistPlayCount <- dbGetQuery(conn=con, statement=artistPlayCountSelect)
-barplotCount(artistPlayCount, file="Artist_Play_Count.png")
+barplotCount(artistPlayCount, file="Artist_Play_Count.png", title="Most Played Artists")
 
-ratingCountSelect <- "SELECT t.Rating, count(1) AS count FROM CoreTracks t WHERE t.Rating > 0 GROUP BY t.Rating;"
+ratingCountSelect <- "SELECT t.Rating, count(1) AS count FROM CoreTracks t WHERE t.Rating > 0 AND t.PrimarySourceID = 1 GROUP BY t.Rating;"
 ratingCount <- dbGetQuery(conn=con, statement=ratingCountSelect)
-barplotCount(ratingCount, file="Rating_Count.png", count=5)
+barplotCount(ratingCount, file="Rating_Count.png", count=5, title="Count of Tracks Per Rating")
 
-yearCountSelect <- "SELECT t.year, count(1) AS count FROM CoreTracks t WHERE t.year > 0 GROUP BY t.year;"
+yearCountSelect <- "SELECT t.year, count(1) AS count FROM CoreTracks t WHERE t.year > 0 AND t.PrimarySourceID = 1 GROUP BY t.year;"
 yearCount <- dbGetQuery(conn=con, statement=yearCountSelect)
-barplotCount(yearCount, file="Year_Count.png", count=length(yearCount[,1]))
+barplotCount(yearCount, file="Year_Count.png", count=length(yearCount[,1]), title="Count of Tracks Per Year")
 
-
-avgArtistRatingSelect <- "select a.Name, avg(t.Rating) as Rating, count(1) as Count from coretracks t left join coreartists a on a.artistid = t.artistid group by a.artistid order by Rating desc, Count DESC;"
+avgArtistRatingSelect <- "SELECT a.Name, round(avg(t.Rating),2) as Rating, count(1) AS Count FROM coretracks t LEFT JOIN coreartists a ON a.artistid = t.artistid where a.ArtistId in (Select ArtistId FROM (Select a1.ArtistID, COUNT(1) from CoreArtists a1 left join CoreTracks t1 on t1.ArtistId = a1.ArtistId WHERE t1.PrimarySourceID = 1 GROUP BY a1.ArtistID ORDER BY 2 DESC LIMIT 10)) AND t.PrimarySourceID = 1 GROUP BY a.artistid ORDER BY Rating desc, Count DESC;"
 avgArtistRating <- dbGetQuery(conn=con, statement=avgArtistRatingSelect)
-barplotCount(avgArtistRating, file="Average_Artist_Rating.png")
+barplotCount(avgArtistRating, file="Average_Artist_Rating.png", title="Average Rating per Artist")
+
+png(file="Artist_Ratings.png",width=800,height=800)
+artistRatingsSelect <- "SELECT t.Rating, a.Name FROM coretracks t LEFT JOIN coreartists a ON a.ArtistID = t.ArtistID WHERE t.Rating > 0 AND a.ArtistId in (Select ArtistId FROM (Select a1.ArtistID, COUNT(1) from CoreArtists a1 left join CoreTracks t1 on t1.ArtistId = a1.ArtistId AND t1.PrimarySourceID = 1 GROUP BY a1.ArtistID ORDER BY 2 DESC LIMIT 10)) AND t.PrimarySourceID = 1 ORDER BY a.Name ASC;"
+artistRatings <- dbGetQuery(conn=con, statement=artistRatingsSelect)
+boxplot(artistRatings$Rating~artistRatings$Name, ylim=c(0,5), las=2)
+graphics.off()
